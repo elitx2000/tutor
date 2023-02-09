db4793820239e77d0d144c6873e4237179b2d1b3
Elijah <elitx2000@gmail.com>
2023-02-09 10:32:22 -0600

    project initialization

---
+++ src/main/java/com/tutor/tutor/TutorApplication.java
@@ -0,0 +1,13 @@
+package com.tutor.tutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutor/tutor/TutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutor.tutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
